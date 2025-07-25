0652f0d042095e44e873deaaf1e177903a4e3536
pyare007 <mailtopyaremohan@gmail.com>
2025-07-26 03:03:17 +0530

    Added a new endpoint

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello All, Learning CI/CD on GCP! It was a nice learning...'
+    return 'Hello All, It is a GCP CI/CD implementation...'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
